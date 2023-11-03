549526f2fe858c66f5915730d055c98d633dd37f
loyz <a@loyz.de>
2023-11-03 19:45:16 +0100

    fix indent

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,5 +1,5 @@
 """
-Django command to wait for db before connecting
+Django command to wait for db before connecting.
 """
 from django.core.management.base import BaseCommand
 
@@ -11,20 +11,19 @@ from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
-        """Django command to wait for db"""
-        def handle(self, *args, **options):
-            """Command Entrypoint"""
-            self.stdout.write('Waiting for db ....')
-            db_up = False
-            while db_up is False:
-                try:
-                        self.check(databases=['default'])
-                        db_up = True
-                except (Psycopg2OpError, OperationalError):
-                    self.stdout.write('Database unavailable, waiting 1 second ...')
-                    time.sleep(1)
-
-            self.stdout.write(self.style.SUCCESS('Database available!'))
+    """Django command to wait for db."""
+    def handle(self, *args, **options):
+       """Command Entrypoint"""
+       self.stdout.write('Waiting for db ....')
+       db_up = False
+       while db_up is False:
+           try:
+                self.check(databases=['default'])
+                db_up = True
+           except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second ...')
+                time.sleep(1)
+       self.stdout.write(self.style.SUCCESS('Database available!'))
 
 
 
