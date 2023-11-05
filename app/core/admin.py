ac482a4a4f5e7402535cf6675097c3adb232fff9
loyz <a@loyz.de>
2023-11-05 11:09:13 +0100

    customize admin add user page

--- app/core/admin.py
+++ app/core/admin.py
@@ -27,6 +27,20 @@ class UserAdmin(BaseUserAdmin):
         (_('Important date'), {'fields': ('last_login',)})
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
