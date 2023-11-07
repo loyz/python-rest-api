6f67556cde77ccf8db5180554ec57e8601970d40
loyz <a@loyz.de>
2023-11-07 18:03:00 +0100

    resolve linting errors

--- app/core/models.py
+++ app/core/models.py
@@ -96,11 +96,11 @@ class Translation(models.Model):
     """save() method is overridden to check if translation_input is present
         and translation_elements is empty. If this condition is true, the input is parsed using Beautiful Soup
         and converted to a list of strings using a list comprehension."""
-    # def save(self, *args, **kwargs):
-    #     if self.translation_input and not self.translation_elements:
-    #         self.translation_elements = self.get_soup_content()
-    #         self.translation_elements = self.filter_and_translate_html(self.translation_elements)
-    #     super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if self.translation_input and not self.translation_elements:
+            self.translation_elements = self.get_soup_content()
+            self.translation_elements = self.filter_and_translate_html(self.translation_elements)
+        super().save(*args, **kwargs)
 
     def get_soup_content(self):
         soup = BeautifulSoup(self.translation_input, 'html.parser')
@@ -132,5 +132,3 @@ class Translation(models.Model):
             'translation_elements': self.translation_elements,
             'translation_result': self.translation_result,
         })
-
-
